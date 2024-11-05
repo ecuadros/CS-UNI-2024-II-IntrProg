309eb5ae2e894421135044897a70e39c91e9ec67
Sanchez Villagaray Jhon Gabriel <jhon.sanchez.v@uni.pe>
2024-11-05 14:34:59 -0500

    Update sort.cpp
    cambio 10 a 9 en los límites

--- sort.cpp
+++ sort.cpp
@@ -40,6 +40,6 @@ void Print(int arr[], int tamaño) {
 
 void SortDemo(){
     int v[10] = {5,2,10,70,45, 8, 9, 7, 15, 23};
-    QuickSort(v, 0, 10);
-    Print(v, 10);
+    QuickSort(v, 0, 9);
+    Print(v, 9);
 }
