f1f5967ee1064d12f93bb77115e6cf0a5535f3a3
WazaChairmound <wazagolf@gmail.com>
2024-11-06 21:46:41 -0500

    Mod 06/11/24 2

--- sort.cpp
+++ sort.cpp
@@ -2,7 +2,7 @@
 
 void SortDemo(){
     int v[10] = {5,2,10,70,45, 8, 9, 7, 15, 23};
-    QuickSort(v, 0, 9,true);
+    QuickSort(v, 0, 9,false);
     Print(cout, v, 10);
 
     float w[10] = {5.3, 2.1, 10.5, 70.4, 45.3, 8.7, 9.2, 1.5, 2.5, 2.3};
