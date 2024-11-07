6274804d19db1394a3f9de56f4bbd8ff9fb0ddae
Gabriel R. <gabrielroblesgamarra@gmail.com>
2024-11-07 02:21:45 -0500

    Update sort.h
    Actualización de rama 18

--- sort.h
+++ sort.h
@@ -5,16 +5,20 @@
 using namespace std;
 
 template <typename T>
-int partition(T arr[], int low, int high) {
+int partition(T arr[], int low, int high, bool ascending = true) { // uso de un cuarto parámetro (bool)
     T pivot = arr[low]; // Pivote como el primer elemento
     int i = low + 1;
     int j = high;
 
     while (true) {
-        // Por la izq deben estar los menores 
+        if (ascending) {
         while (i <= j && arr[i] <= pivot)   i++;
-        // Por la der deben estar los mayores
         while (i <= j && arr[j] > pivot)    j--;
+        }
+        else {
+            while (i <= j && arr[i] >= pivot) i++;
+            while (i <= j && arr[j] < pivot) j--;
+        }
         // Se cruzaron los índices, salir del bucle
         if (i > j)  break; 
         std::swap(arr[i], arr[j]);  // Intercambiar elementos
@@ -25,11 +29,11 @@ int partition(T arr[], int low, int high) {
 }
 
 template <typename T>
-void QuickSort(T arr[], int low, int high) {
+void QuickSort(T arr[], int low, int high, bool ascending = true) {
     if (low < high) {
-        int pi = partition(arr, low, high);
-        QuickSort(arr, low, pi -1);
-        QuickSort(arr, pi + 1, high);
+        int pi = partition(arr, low, high, ascending);
+        QuickSort(arr, low, pi -1, ascending);
+        QuickSort(arr, pi + 1, high, ascending);
     }
 }
 
