fa710cb958c3637bee73c791073c718574a4f8dd
Sir.Caleb <leoncaleb1@outlook.es>
2024-11-12 15:59:49 -0500

    Segunda modificación a la función lambda dentro de la llamada a la función quicksort que ordena alumnos

--- sort.cpp
+++ sort.cpp
@@ -40,7 +40,13 @@ void SortDemo(){
     cout << "*******************************\n";
 
     cout << "Ascending Sort by mm/dd:" << endl;
-    QuickSort(vAlu, 0, 9, [](Alu &a, Alu &b){ return (a.mm >= b.mm) && (a.mm > b.mm || a.dd > b.dd); }); //forma equivalente
+    QuickSort(vAlu, 0, 9, [](Alu &a, Alu &b){ return a.mm > b.mm || (a.mm == b.mm && a.dd > b.dd); });
+    Print(cout, vAlu, 10, "\n");
+    cout << "*******************************\n";
+
+    cout << "Descending Sort by mm/dd:" << endl;
+    MyComp comp;
+    QuickSort(vAlu, 0, 9, comp);
     Print(cout, vAlu, 10, "\n");
     cout << "*******************************\n";
 }
