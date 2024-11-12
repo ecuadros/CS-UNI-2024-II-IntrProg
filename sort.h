a27ea17e2a68566dd209b227858ec804084bd9c6
JesusY736 <jesusyaulimarca8@gmail.com>
2024-11-12 12:29:40 -0500

    Update sort.h
    quicksort

--- sort.h
+++ sort.h
@@ -1,41 +1,49 @@
-#ifndef __QUICK_SORT_H__
-#define __QUICK_SORT_H__
+#ifndef _QUICK_SORT_H_
+#define _QUICK_SORT_H_
 #include <iostream>
-#include <algorithm>    // std::swap
+#include <algorithm>    // swap
+#include <cstddef>      // size_t
 using namespace std;
 
+// Función de comparación para decidir si se ordenará de manera ascendente o descendente
 template <typename T>
-int partition(T arr[], int low, int high) {
+bool compare(T a, T b) {
+    return a > b;  // "<": Ascendente -- ">": Descendente
+}
+
+template <typename T>
+size_t partition(T arr[], size_t low, size_t high) {
     T pivot = arr[low]; // Pivote como el primer elemento
-    int i = low + 1;
-    int j = high;
+    size_t i = low + 1;
+    size_t j = high;
 
     while (true) {
-        // Por la izq deben estar los menores
-        while (i <= j && arr[i] <= pivot)   i++;
-        // Por la der deben estar los mayores
-        while (i <= j && arr[j] > pivot)    j--;
+        // Por la izquierda deben estar los menores
+        while (i <= j && compare(arr[i], pivot))   i++;
+        // Por la derecha deben estar los mayores
+        while (i <= j && !compare(arr[j], pivot))  j--;
         // Se cruzaron los índices, salir del bucle
         if (i > j)  break; 
-        std::swap(arr[i], arr[j]);  // Intercambiar elementos
+        swap(arr[i], arr[j]);  // Intercambiar elementos
         i++;    j--;
     }
-    std::swap(arr[low], arr[j]);    // Colocar el pivote en su lugar
+    swap(arr[low], arr[j]);    // Colocar el pivote en su lugar
     return j; // Retornar la posición del pivote
 }
 
 template <typename T>
-void QuickSort(T arr[], int low, int high) {
+void QuickSort(T arr[], size_t low, size_t high) {
     if (low < high) {
-        int pi = partition(arr, low, high);
-        QuickSort(arr, low, pi -1);
+        size_t pi = partition(arr, low, high);
+        // Llamadas recursivas
+        if (pi > 0) QuickSort(arr, low, pi - 1);  // Verifica que pi > 0 para evitar underflow
         QuickSort(arr, pi + 1, high);
     }
 }
 
 template <typename T>
-void Print(ostream &os, T arr[], int size) {
-    for (int i = 0; i < size; i++) {
+void Print(ostream &os, T arr[], size_t size) {
+    for (size_t i = 0; i < size; i++) {
         os << arr[i] << " ";
     }
     os << endl;
@@ -43,4 +51,4 @@ void Print(ostream &os, T arr[], int size) {
 
 void SortDemo();
 
-#endif  // __QUICK_SORT_H__
+#endif  // _QUICK_SORT_H_
