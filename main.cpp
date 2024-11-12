45ee9bc7a8161b43f859e17ee22efd6966ced924
AcevedoGod <almonacidsullcas5@gmail.com>
2024-11-12 13:08:13 -0600

    Forma descendente del arreglo

--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+#include "sort.h"
+
+
 using namespace std;
 
+// g++ -std=c++17 main.cpp pointers.cpp sorting.cpp sort.cpp files.cpp vector.cpp bits.cpp struct.cpp -o main
+
+// g++ -std=c++17 main.cpp sort.cpp -o main
+
 int main(){
-    cout << "Hello Introd Programacion CS-UNI";
+    cout << "Hello Introd Programacion CS-UNI\n";
+ 
+    SortDemo();
+   
 	return 0;
 }
