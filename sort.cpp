271dc850762aa0e5e78bd4c797efc95084ab9da8
AcevedoGod <almonacidsullcas5@gmail.com>
2024-11-14 13:01:23 -0600

    Datos Actualizados

--- sort.cpp
+++ sort.cpp
@@ -2,10 +2,11 @@
 
 void SortDemo(){
     int v[10] = {5,2,10,70,45, 8, 9, 7, 15, 23};
-    QuickSort(v, 0, 9);
+    bool ascendente = true; //false = descendente
+    QuickSort(v, 0, 9, ascendente);
     Print(cout, v, 10);
 
     float w[10] = {5.3, 2.1, 10.5, 70.4, 45.3, 8.7, 9.2, 1.5, 2.5, 2.3};
-    QuickSort(w, 0, 9);
+    QuickSort(w, 0, 9, ascendente);
     Print(cout, w, 10);
 }
--- sort.h
+++ sort.h
@@ -5,16 +5,27 @@
 using namespace std;
 
 template <typename T>
-int partition(T arr[], int low, int high) {
+int partition(T arr[], int low, int high, bool ascendente) {
     T pivot = arr[low]; // Pivote como el primer elemento
     int i = low + 1;
     int j = high;
 
     while (true) {
-        // Por la izq deben estar los menores 
-        while (i <= j && arr[i] >= pivot)   i++;
-        // Por la der deben estar los mayores
-        while (i <= j && arr[j] < pivot)    j--;
+        if (ascendente)
+        {
+            // Por la izq deben estar los menores 
+            while (i <= j && arr[i] <= pivot)   i++;
+            // Por la der deben estar los mayores
+            while (i <= j && arr[j] > pivot)    j--;
+        }
+        else
+        {
+            // Por la izq deben estar los mayores
+            while (i <= j && arr[i] >= pivot)   i++;
+            // Por la der deben estar los menores
+            while (i <= j && arr[j] < pivot)    j--;
+        }
+        
         // Se cruzaron los índices, salir del bucle
         if (i > j)  break; 
         std::swap(arr[i], arr[j]);  // Intercambiar elementos
@@ -25,11 +36,11 @@ int partition(T arr[], int low, int high) {
 }
 
 template <typename T>
-void QuickSort(T arr[], int low, int high) {
+void QuickSort(T arr[], int low, int high, bool ascendente = true) {
     if (low < high) {
-        int pi = partition(arr, low, high);
-        QuickSort(arr, low, pi -1);
-        QuickSort(arr, pi + 1, high);
+        int pi = partition(arr, low, high, ascendente);
+        QuickSort(arr, low, pi -1, ascendente);
+        QuickSort(arr, pi + 1, high, ascendente);
     }
 }
 
