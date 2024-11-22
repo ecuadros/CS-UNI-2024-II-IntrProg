8a81d4ac3f1991d2bf9b23be5656503d31888f9d
AcevedoGod <almonacidsullcas5@gmail.com>
2024-11-21 23:25:19 -0600

    Código de la clase del 21/11/2024

---
+++ Draw/circle.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "circle.h"
+
+using namespace std;
+
+void Circle::draw(){
+    cout<<"Circle draw"<<endl;
+}
---
+++ Draw/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include "figure.h"
+#include "circle.h"
+#include "point.h"
+#include "rectangle.h"
+
+using namespace std;
+
+// g++ -std=c++17 main.cpp figure.cpp rectangle.cpp circle.cpp point.cpp -o main
+
+void fx(Figure *p)
+{
+    p->draw();
+}
+void fy(Figure &rObj)
+{
+    rObj.draw();
+}
+
+int main(){
+    Figure *pf[3]= {new Rectangle, 
+                    new Circle,
+                    new Point};
+    
+    for (auto i = 0; i < 3; i++)
+    {
+        pf[i]->draw();
+    }
+    
+    /* for (auto i = 0; i < 3; i++)
+    {
+        fx(pf[i]);
+    } */
+    
+    /* for (auto i = 0; i < 3; i++)
+    {
+        fy(*pf[i]);
+    } */
+
+}
