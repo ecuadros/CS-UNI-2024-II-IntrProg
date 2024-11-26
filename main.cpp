6cafa9970100745e4741bb1380c4dbd47f1b7c44
Cristhyan Perez <pevicristhyan@gmail.com>
2024-11-26 11:24:20 -0500

    Nombre: Cristhyan Perez Villegas - Tarea: Polimorfismo

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
+#include "figure.h"
+#include "rectangle.h"
+#include "circle.h"
+#include "demo.h"
 using namespace std;
 
 int main(){
     cout << "Hello Introd Programacion CS-UNI";
+    cout << "\n";
+    demo();
 	return 0;
 }
