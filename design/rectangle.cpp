12b7d24b5bce5f71654777c615b9147ede174252
JesusY736 <jesusyaulimarca8@gmail.com>
2024-12-02 23:41:56 -0500

    Update rectangle.cpp

--- design/rectangle.cpp
+++ design/rectangle.cpp
@@ -2,6 +2,16 @@
 #include "rectangle.h"
 #include "funcion.h"
 using namespace std;
+Rectangle::Rectangle(string _name)
+          :Figure(_name)
+{
+    cout << "Rectangle: " << GetName() << " Construido ..." << endl;
+}
+
+Rectangle::~Rectangle()
+{
+    cout << "Rectangle: " << GetName() << " Destruido ..." << endl;
+}
 
 void Rectangle::draw()
 {
