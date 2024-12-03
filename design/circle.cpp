11f788814a6753396d2339e8839f17f1f7c1d9c3
BrayanCocaH <brayancocah@gmail.com>
2024-12-02 22:44:38 -0500

    Update circle.cpp

--- design/circle.cpp
+++ design/circle.cpp
@@ -2,6 +2,10 @@
 #include "circle.h"
 
 using namespace std;
+Circle::Circle(const std::string &name) : Figure(name)
+{
+    cout << "Circle: " << GetName() << " Constructor llamado ..." << endl;
+}
 void Circle::draw()
 {
     Figure::draw();
