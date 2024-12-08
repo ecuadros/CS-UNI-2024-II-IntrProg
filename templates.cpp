17a6ba13fe970a882e4f46420ee8ee191dea1660
ecuadros <ecuadros@spc.org.pe>
2024-12-08 17:13:46 -0500

    Ajustes menores

--- templates.cpp
+++ templates.cpp
@@ -5,15 +5,16 @@ void DemoVarTemplates(){
     double d = 3.5;
     long   l = 5;
 
-    auto rpta1 = suma(i, d, l);
+    auto rpta1 = suma(i, d, l); // Imprime: 10.5
     cout << rpta1 << endl;
 
     auto rpta2 = suma(d, 3, l, 4.3, i);
-    cout << rpta2 << endl;
+    cout << rpta2 << endl;      // Imprime: 17.8
 
     string text1 = "Hola",
            text2 = "C++",
-           text3 = "UNI";
+           text3 = "UNI:",
+           text4 = "llego Navidad";
     auto rpta3 = suma(text1, text2, text3);  
-    cout << rpta3 << endl;    
+    cout << rpta3 << endl;    // Imprime: HolaC++UNI:llego Navidad
 }
