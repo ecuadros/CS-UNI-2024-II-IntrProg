54921b1df98b564956e37823efabffbf8f951fac
elmerolivo <elmerolivo2412@gmail.com>
2024-12-14 18:49:35 -0500

    agregamos la libreria sstream para utilizar toString

--- templates.h
+++ templates.h
@@ -2,13 +2,24 @@
 #define __VARIADIC_TEMPLATES_H__
 #include <iostream>
 #include <string>
+#include <sstream>
 using namespace std;
 
 template<typename T>
 auto suma(const T& value) {
     return value;
 }
-
+// Sobrecarga para manejar un tipo de dato no string
+//toString: convierte cualquier tipo de dato en una representación de tipo string
+string toString(const string& value) {
+    return value;
+}
+template<typename T>
+string toString(const T& value) {
+    ostringstream oss;
+    oss << value;
+    return oss.str();
+}
 
 template<typename T, typename ...Args>
 auto suma(const T& value, const Args&... args) {
