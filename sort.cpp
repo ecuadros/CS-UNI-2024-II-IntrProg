d634b28dc74d7851153e1183b2f7be7837e4bc80
↪FernandoChirinos <fernando04042024@gmail.com>
2024-12-13 23:28:09 -0500

    Asc or desc en SortDemo()

--- sort.cpp
+++ sort.cpp
@@ -1,11 +1,11 @@
 #include "sort.h"
 
-void SortDemo(){
+void SortDemo(bool ascending){
     int v[10] = {5,2,10,70,45, 8, 9, 7, 15, 23};
-    QuickSort(v, 0, 9);
+    QuickSort(v, 0, 9, ascending);
     Print(cout, v, 10);
 
     float w[10] = {5.3, 2.1, 10.5, 70.4, 45.3, 8.7, 9.2, 1.5, 2.5, 2.3};
-    QuickSort(w, 0, 9);
+    QuickSort(w, 0, 9, ascending);
     Print(cout, w, 10);
 }
