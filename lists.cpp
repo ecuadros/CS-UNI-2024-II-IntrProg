29e1dd3f1533d8b4f0e62b6b3b1f74263ab5af2d
↪FernandoChirinos <fernando04042024@gmail.com>
2024-12-14 00:20:19 -0500

    Comentarios liberados
    a excepción de los que se deben descomentar para ser usados (ultimo bloque, según necesidad)

--- lists.cpp
+++ lists.cpp
@@ -6,76 +6,86 @@ const int nElem = 10;
 
 template <typename T>
 void print(T &x)
-{  cout << x << "  "; }
+{  
+    std::cout << x << "  "; 
+}
 
 // Object function
 template <typename T>
 class ClassX
-{          int m_inc = 0;
-    public:  ClassX(int n) : m_inc(n){}
-    void operator()(T &n){  n += m_inc;     }
+{          
+    int m_inc = 0;
+public:  
+    ClassX(int n) : m_inc(n) {}
+    void operator()(T &n) {  n += m_inc; }
 };
 
 template <typename T>
-void fx(T &n) {    n++;    }
+void fx(T &n) {    
+    n++;    
+}
 
 template <typename Container>
 void DemoLinkedList(Container &mylist){
     using T = typename Container::value_type;
 
-    for(T x=0; x<nElem; x++)
+    for(T x = 0; x < nElem; x++)
        mylist.insert(x);
-    // TODO: liberar os siguientes comentarios y hacerlos correr
+
     // Lambda function
-    // foreach(myAscList, [x](TX &n){ n += x; x++; });
-    // foreach(myAscList, ::print<TX>); cout << endl;
-    // ClassX<TX> ope(5);
-    // foreach(myAscList, ope);
-    // foreach(myAscList, ::print<TX>); cout << endl;
-    // foreach(myAscList, ClassX<TX>(8) );
-    // foreach(myAscList, ::print<TX>); cout << endl;
-    
-    foreach(mylist, ::print<T>);  cout << endl;
+    foreach(mylist, [x](T &n) mutable { n += x; x++; }); // Capturing x by value and mutably
+    foreach(mylist, ::print<T>); 
+    std::cout << std::endl;
+
+    // Object function
+    ClassX<T> ope(5);
+    foreach(mylist, ope);
+    foreach(mylist, ::print<T>);
+    std::cout << std::endl;
+
+    // Using the object function with a different increment
+    foreach(mylist, ClassX<T>(8));
+    foreach(mylist, ::print<T>);
+    std::cout << std::endl;
 }
 
 void DemoLinkedListSorted(){
-    cout << "Ascending list" << endl;
-    LinkedList< LLTraitAsc<TX> > myAscList;
+    std::cout << "Ascending list" << std::endl;
+    LinkedList< LLTraitAsc<int> > myAscList; // Change `TX` to `int` or your container type
     DemoLinkedList(myAscList);
-    // foreach(myAscList);
 
-    // cout << "Descending list" << endl;
-    // LinkedList< LLTraitDesc<TX> > myDescList;
-    // demoLinkedList(myDescList);
-    // foreach(myDescList);
+    // Uncomment and use for descending list
+    std::cout << "Descending list" << std::endl;
+    LinkedList< LLTraitDesc<int> > myDescList;
+    DemoLinkedList(myDescList);
 }
 
 // template <typename Container>
 // void demoDoubleLinkedList(Container &mylist)
 // {
-//     cout << "Inserting:       ";
+//     std::cout << "Inserting:       ";
 //     for(auto x=0; x<nElem; x++)
 //     {   
-//       cout << vect[x] << ", "; 
+//       std::cout << vect[x] << ", "; 
 //       mylist.insert(vect[x]);
 //       //mylist.push_back(vect[x]);
 //     }
-//     cout << endl;
-//     cout << "Lista en orden : ";
+//     std::cout << std::endl;
+//     std::cout << "Lista en orden : ";
 //     using T = typename Container::value_type;
-//     foreach(mylist, fx<T>);  cout << endl;
+//     foreach(mylist, fx<T>);  std::cout << std::endl;
     
-//     cout << "Lista invertida: ";
-//     foreach_inverso(mylist, fx<T>);  cout << endl;
+//     std::cout << "Lista invertida: ";
+//     foreach_inverso(mylist, fx<T>);  std::cout << std::endl;
 // }
 
 // void demoDoubleLinkedListSorted()
 // {
-//     cout << "Ascending double list" << endl;
-//     DoubleLinkedList< DLLAscTraits<TX> > myAscList;
+//     std::cout << "Ascending double list" << std::endl;
+//     DoubleLinkedList< DLLAscTraits<int> > myAscList;
 //     demoDoubleLinkedList(myAscList);
 
-//     cout << "Descending double list" << endl;
-//     DoubleLinkedList< DLLDescTraits<TX> > myDescList;
+//     std::cout << "Descending double list" << std::endl;
+//     DoubleLinkedList< DLLDescTraits<int> > myDescList;
 //     demoDoubleLinkedList(myDescList); 
 // }
