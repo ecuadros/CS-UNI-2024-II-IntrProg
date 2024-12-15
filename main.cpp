3bccc0f27f017242cf87bef674e7a56c373c7c5c
OrtizBrad <krisbrad2406@gmail.com>
2024-12-15 14:17:09 -0500

    TRABAJO FINAL - ORTIZ BRAD
    Este es el archivo Cell.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,23 @@
-#include <iostream>
-using namespace std;
+#include "Cell.h"
+#include <stdexcept>
 
-int main(){
-    cout << "Hello Introd Programacion CS-UNI";
-	return 0;
+// Constructor
+Cell::Cell(const std::string& value) : content(value) {}
+
+// Verifica si el contenido es un número
+bool Cell::isNumber() const {
+    try {
+        std::stod(content);
+        return true;
+    } catch (...) {
+        return false;
+    }
+}
+
+// Evalúa el contenido si es un número
+double Cell::evaluate() const {
+    if (isNumber()) {
+        return std::stod(content);
+    }
+    return 0;
 }
