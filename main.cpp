7c9e6d2ff3e9b483eb9e0b81574ae95223e9e8ec
OrtizBrad <krisbrad2406@gmail.com>
2024-12-15 17:24:33 -0500

    TRABAJO FINAL 2 - ORTIZ BRAD
    Archivo Excel.h

--- main.cpp
+++ main.cpp
@@ -1,7 +1,25 @@
-#include <iostream>
-using namespace std;
+#ifndef EXCEL_H
+#define EXCEL_H
 
-int main(){
-    cout << "Hello Introd Programacion CS-UNI";
-	return 0;
-}
+#include <vector>
+#include <string>
+
+class Excel {
+public:
+    // Constructor
+    Excel(int filas, int columnas);
+
+    // Métodos
+    void ingresarDato(int fila, int columna, double dato);
+    double obtenerDato(int fila, int columna) const;
+    void mostrarHoja() const;
+    void sumarFilas(int fila) const;
+    void sumarColumnas(int columna) const;
+
+private:
+    // Representación de la hoja de cálculo (matriz)
+    std::vector<std::vector<double>> hoja;
+};
+
+#endif // EXCEL_H
+#pragma once
