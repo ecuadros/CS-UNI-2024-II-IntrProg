f5a957f2757db71941a5ce27ba5f4f0334824abc
Gabriel R. <gabrielroblesgamarra@gmail.com>
2024-12-15 20:30:50 -0500

    Update templates.h

--- templates.h
+++ templates.h
@@ -2,17 +2,25 @@
 #define __VARIADIC_TEMPLATES_H__
 #include <iostream>
 #include <string>
+#include <sstream> // stringstream
 using namespace std;
 
 template<typename T>
-auto suma(const T& value) {
-    return value;
+string to_str(const T &value) {
+    ostringstream oss;
+    oss << value;
+    return oss.str();
+}
+
+template<typename T>
+auto suma(const T& value) { // conversión
+    return to_str(value);
 }
 
 
-template<typename T, typename ...Args>
-auto suma(const T& value, const Args&... args) {
-    return value + suma(args...); 
+template<typename T, typename ...Args> // variadic template
+auto suma(const T& value, const Args&... args) { // versión "recursiva" de suma
+    return to_str(value) + suma(args...); 
 }
 
 void DemoVarTemplates();
