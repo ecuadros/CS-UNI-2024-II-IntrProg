d5f2046f24c589110798d356d2bea45f26efed04
Gabriel R. <gabrielroblesgamarra@gmail.com>
2024-12-15 19:55:05 -0500

    Update rectangle.cpp

--- design/rectangle.cpp
+++ design/rectangle.cpp
@@ -2,17 +2,12 @@
 #include "rectangle.h"
 #include "funcion.h"
 using namespace std;
-Rectangle::Rectangle(string _name)
-          :Figure(_name)
-{
-    cout << "Rectangle: " << GetName() << " Construido ..." << endl;
+Rectangle::Rectangle(string _name = "Default Rectangle") : Figure(_name) {
+cout << "Rectangle:" << GetName() << "Construido... " << endl;
 }
-
-Rectangle::~Rectangle()
-{
-    cout << "Rectangle: " << GetName() << " Destruido ..." << endl;
+Rectangle::~Rectangle() {
+cout << "Rectangle:" << GetName() << "Destruido ... " << endl;
 }
-
 void Rectangle::draw()
 {
     Figure::draw();
