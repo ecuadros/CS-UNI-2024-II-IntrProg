38224f660d91ed7a3fa988b9bc483da81a4a1f0e
↪FernandoChirinos <fernando04042024@gmail.com>
2024-12-15 21:52:50 -0500

    Templates Variadicos

--- templates.h
+++ templates.h
@@ -1,20 +1,36 @@
 #ifndef __VARIADIC_TEMPLATES_H__
 #define __VARIADIC_TEMPLATES_H__
+
 #include <iostream>
+#include <sstream>
 #include <string>
-using namespace std;
 
+// Función base: convierte cualquier valor a std::string
 template<typename T>
-auto suma(const T& value) {
-    return value;
+std::string to_string(const T& value) {
+    std::ostringstream oss;
+    oss << value; // Convierte el valor a texto
+    return oss.str();
 }
 
-
-template<typename T, typename ...Args>
-auto suma(const T& value, const Args&... args) {
-    return value + suma(args...); 
+// Función base para <<suma>>: caso único (cuando queda un solo argumento)
+template<typename T>
+std::string suma(const T& value) {
+    return to_string(value); // Convierte el valor restante a cadena
 }
 
-void DemoVarTemplates();
+// Función recursiva para <<suma>>: combina valores usando concatenación
+template<typename T, typename... Args>
+std::string suma(const T& value, const Args&... args) {
+    return to_string(value) + suma(args...); // Concatena el valor actual con el resto
+}
 
-#endif //
+// Función de demostración
+void DemoVarTemplates() {
+    std::cout << "Concatenar cadenas y números:" << std::endl;
+    std::cout << suma("La suma de ", 5, " y ", 10, " es ", 15) << std::endl;
+    std::cout << suma("Mi edad es ", 25, ".") << std::endl;
+    std::cout << suma("Pi es aproximadamente ", 3.14159) << std::endl;
+}
+
+#endif // __VARIADIC_TEMPLATES_H__
